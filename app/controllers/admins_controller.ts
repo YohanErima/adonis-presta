5515287446521da1fb6e7f3b298891a08ede82a6
YohanErima <y.erima@orange.fr>
2024-03-20 12:02:24 +0100

    update controllers and routes

--- app/controllers/admins_controller.ts
+++ app/controllers/admins_controller.ts
@@ -1,5 +1,7 @@
 import User from '#models/user'
+import { cuid } from '@adonisjs/core/helpers'
 import type { HttpContext } from '@adonisjs/core/http'
+import fs from 'fs'
 
 export default class AdminsController {
   async getUsers({ response }: HttpContext) {
@@ -7,13 +9,19 @@ export default class AdminsController {
     return response.ok(users)
   }
 
-  async getUserByEmail({ request, response }: HttpContext) {
-    const user = await User.findBy('email', request.all().email)
+  async getUserById({ request, response }: HttpContext) {
+    const { id } = request.params()
+    const user = await User.find(id)
     return response.ok(user)
   }
   async patchUserById({ request, response }: HttpContext) {
     const { id } = request.params()
-    const { email, fullname, area, job, tel } = request.all()
+    const email = request.input('email')
+
+    const fullname = request.input('fullname')
+    const area = request.input('area')
+    const tel = request.input('tel')
+    const job = request.input('job')
 
     const user = await User.find(id)
     if (!user) {
@@ -36,15 +44,23 @@ export default class AdminsController {
       user.tel = tel
     }
 
-    await user.save()
+    try {
+      await user.save()
 
-    return response.ok({ message: 'User updated' })
+      return response.ok({ message: 'User updated' })
+    } catch (error) {
+      console.log(error)
+      response.internalServerError({ message: 'Error Server' })
+    }
   }
 
   async patchImgById({ request, response }: HttpContext) {
-    const { img } = request.all()
+    const img = request.file('img', {
+      extnames: ['jpg', 'png', 'jpeg'],
+      size: '4mb',
+    })
     const { id } = request.params()
-    if (!img) {
+    if (!img || !img.isValid) {
       return response.badRequest({ message: 'You need a image!' })
     }
     const user = await User.find(id)
@@ -52,10 +68,26 @@ export default class AdminsController {
     if (!user) {
       return response.badRequest({ message: 'Error with the User' })
     }
+    const filename = `${cuid()}.${img.extname}`
+    const oldFile = user.img
+    try {
+      if (fs.existsSync(`public/uploads/${oldFile}`)) {
+        fs.unlinkSync(`public/uploads/${oldFile}`)
+      }
+      user.img = filename
+
+      await user.save()
+      await img.move('public/uploads', { name: filename })
+
+      return response.ok({ message: 'User image updated' })
+    } catch (error) {
+      console.log(error)
+      return response.badRequest({ message: 'Error' })
+    }
   }
   async patchPasswordById({ request, response }: HttpContext) {
     const { id } = request.params()
-    const { password } = request.all()
+    const password = request.input('password')
     if (!password) {
       return response.badRequest({ message: 'You need a new password!' })
     }
@@ -63,5 +95,36 @@ export default class AdminsController {
     if (!user) {
       return response.badRequest({ message: 'Error with the User' })
     }
+
+    try {
+      user.password = password
+      user.save()
+      return response.ok({ message: 'Password updated!' })
+    } catch (error) {
+      console.log(error)
+      return response.internalServerError({ message: 'Error server' })
+    }
+  }
+
+  async deleteUserById({ request, response }: HttpContext) {
+    const { id } = request.params()
+    const user = await User.find(id)
+    if (!user) {
+      return response.badRequest({ message: "User doesn't exist! " })
+    }
+    try {
+      await user.delete()
+      return response.ok('User deleted!')
+    } catch (error) {
+      return response.internalServerError({ message: 'Error server' })
+    }
+  }
+  async enableUserById({ request, response }: HttpContext) {
+    const { id } = request.params()
+    const user = await User.find(id)
+    if (!user) {
+      return response.badRequest({ message: "User doesn't exist! " })
+    }
+    return response
   }
 }
--- app/middleware/admin_check_middleware.ts
+++ app/middleware/admin_check_middleware.ts
@@ -2,25 +2,20 @@ import type { HttpContext } from '@adonisjs/core/http'
 import type { NextFn } from '@adonisjs/core/types/http'
 
 export default class AdminCheckMiddleware {
-  async handle(ctx: HttpContext, next: NextFn) {
+  async handle({ auth, response }: HttpContext, next: NextFn) {
     /**
      * Middleware logic goes here (before the next call)
      */
-    console.log(ctx)
+    const user = await auth?.user
+    console.log('#################################################################', user?.is_admin)
+    if (!user?.is_admin) {
+      response.unauthorized({ message: 'You are not authorized to perform this action' })
+    }
 
     /**
      * Call next method in the pipeline and return its output
      */
     const output = await next()
     return output
-  }
-  async adminCheck({ auth, response }: HttpContext, next: NextFn) {
-    const user = auth.user
-
-    if (!user?.is_admin) {
-      response.unauthorized({ message: 'You are not authorized to perform this action' })
-    }
-
-    await next()
   }
 }
